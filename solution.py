81024681f4028ccc337a0e374b199955689d8daf
juaniyo98 <68514355+juaniyo98@users.noreply.github.com>
2021-09-07 02:14:53 -0400

    Add files via upload

--- solution.py
+++ solution.py
@@ -8,7 +8,7 @@ def welcome_assignment_answers(question):
     #Another way to implement is using a "case" statements similar to C.
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
-        
+
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "Yes"
 
@@ -17,7 +17,7 @@ def welcome_assignment_answers(question):
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "Yes"
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
