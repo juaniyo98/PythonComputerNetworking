4f8c538a94995d071e8cb06c89534f76d5f1000f
juaniyo98 <68514355+juaniyo98@users.noreply.github.com>
2021-10-07 02:10:36 -0400

    Add files via upload

--- solution.py
+++ solution.py
@@ -71,7 +71,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
    # Message ends with a single period.
    # Fill in start
    messageEnd='\r\n.\r\n'
-   clientSocket.send(MessageCommand + messageEnd)
+   clientSocket.send((MessageCommand + messageEnd).encode())
    recv1 = clientSocket.recv(1024).decode()
    print(recv1)
    if recv1[:3] != '250':
