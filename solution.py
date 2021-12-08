ffbc9906166a441d12d55cfbe1975bd788337890
juaniyo98 <68514355+juaniyo98@users.noreply.github.com>
2021-12-08 13:41:26 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -75,7 +75,7 @@ def get_route(hostname):
 
             #Fill in start
             # Make a raw socket named mySocket
-             icmp = getprotobyname("icmp")
+            icmp = getprotobyname("icmp")
           
             mySocket = socket(AF_INET, SOCK_RAW, icmp)
           
