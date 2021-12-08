c6862022133c4e6d8f9dd3bd75f6e9ba0545c6cd
juaniyo98 <68514355+juaniyo98@users.noreply.github.com>
2021-12-07 22:14:55 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -5,6 +5,10 @@ import struct
 import time
 import select
 import binascii
+import statistics
+import statistics.stdev
+import statistics.mean
+
 # Should use stdev
 
 ICMP_ECHO_REQUEST = 8
@@ -113,13 +117,13 @@ def ping(host, timeout=1):
     print("Pinging " + dest + " using Python:")
     print("")
     # Calculate vars values and return them
-    vars = [str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)),str(round(stdev(stdev_var), 2))]
+    #vars = [str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)),str(round(stdev(stdev_var), 2))]
     # Send ping requests to a server separated by approximately one second
     for i in range(0,4):
         delay = doOnePing(dest, timeout)
         print(delay)
         time.sleep(1)  # one second
-
+    vars = [str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)),str(round(stdev(stdev_var), 2))]
     return vars
 
 if __name__ == '__main__':
